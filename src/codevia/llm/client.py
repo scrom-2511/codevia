1ae4362d1e810d654a867564ccce0bca443eadbf
scrom-2511 <formain337@gmail.com>
2026-09-10 22:57:32 +0530

    feat: migrate LLM client to interactions API and add streaming

--- src/codevia/llm/client.py
+++ src/codevia/llm/client.py
@@ -8,9 +8,22 @@ class LLMClient:
     def __init__(self):
         self.client = genai.Client(api_key=os.getenv("GEMINI_API_KEY"))
         
-    def generate_response(self, prompt: str) -> str:
-        response = self.client.models.generate_content(
+    def generate_response(self, prompt: str) -> dict:
+        response = self.client.interactions.create(
             model=os.getenv("GEMINI_MODEL"),
-            contents=prompt,
+            input=prompt
         )
-        return response.text
+        
+        return {"output_text": response.output_text, "previous_interaction_id": response.previous_interaction_id}
+
+    def generate_stream_response(self, prompt: str):
+        stream = self.client.interactions.create(
+            model=os.getenv("GEMINI_MODEL"),
+            input=prompt,
+            stream=True
+        )
+        
+        for event in stream:
+            if event.event_type == "step.delta":
+                if event.delta.type == "text":
+                    print(event.delta.text, end="") # Use yield if another a frontend stream senders calls it
