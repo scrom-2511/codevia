f6e8b2610ed83ec1598e20914a6ad4f692fc0814
scrom-2511 <formain337@gmail.com>
2026-09-11 00:09:36 +0530

    add: VectorDB abstraction for Pinecone

---
+++ src/codevia/database/vector/client.py
@@ -0,0 +1,30 @@
+from typing_extensions import Optional
+from typing import TypedDict
+import pinecone
+
+class Vector(TypedDict):
+    id: str
+    values: list[float]
+    metadata: dict
+
+class VectorDB:
+    def __init__(self, api_key:Optional[str] = None, host:Optional[str] = None):
+        self.api_key = api_key
+        self.host = host
+        if api_key:
+            self.client = pinecone.Pinecone(api_key=api_key, host=host)
+        else:
+            self.client = pinecone.Pinecone(host=host) # connect to the local docker instance
+
+    def create_index(self, index_name:str, dimension:int):
+        if index_name not in self.client.list_indexes().names():
+            self.client.create_index(
+                name=index_name,
+                dimension=dimension,
+                metric="cosine"
+            )
+        return self.client.Index(index_name)
+    
+    def upsert_vectors(self, namespace:str, vectors:list[Vector], index_name:str) -> None:
+        index = self.client.Index(index_name)
+        index.upsert(vectors=vectors, namespace=namespace)
