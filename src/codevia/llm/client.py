0fe6773b3ed20c0717701fa3f0953d682050696c
scrom-2511 <formain337@gmail.com>
2026-09-13 00:11:46 +0530

    add: get_prompt() abstraction

--- src/codevia/llm/client.py
+++ src/codevia/llm/client.py
@@ -7,24 +7,27 @@ load_dotenv()
 class LLMClient:
     def __init__(self):
         self.client = genai.Client(api_key=os.getenv("GEMINI_API_KEY"))
+
+    def get_prompt(self, query: str, context: str) -> str:
+            return f"""
+            You are a codebase assistant.
+
+            Answer the user's question using the provided code context.
+
+            User question:
+            {query}
+
+            Code context:
+            {context}
+
+            Instructions:
+            - Answer based on the provided code.
+            - Mention relevant file paths.
+            - If the context does not contain enough information, say so.
+            """.strip()
         
     def generate_response(self, query: str, context:str) -> dict:
-        prompt = f"""
-        You are a codebase assistant.
-
-        Answer the user's question using the provided code context.
-
-        User question:
-        {query}
-
-        Code context:
-        {context}
-
-        Instructions:
-        - Answer based on the provided code.
-        - Mention relevant file paths.
-        - If the context does not contain enough information, say so.
-        """
+        prompt = self.get_prompt(query, context)
 
         response = self.client.interactions.create(
             model=os.getenv("GEMINI_MODEL"),
@@ -33,7 +36,9 @@ class LLMClient:
         
         return {"output_text": response.output_text, "previous_interaction_id": response.previous_interaction_id}
 
-    def generate_stream_response(self, prompt: str):
+    def generate_stream_response(self, query: str, context:str):
+        prompt = self.get_prompt(query, context)
+
         stream = self.client.interactions.create(
             model=os.getenv("GEMINI_MODEL"),
             input=prompt,
