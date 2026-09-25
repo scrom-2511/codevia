20cd16a12cd623177405d8df58271ace2caf2a28
scrom-2511 <formain337@gmail.com>
2026-09-25 22:57:25 +0530

    refactor(tools/workspace): made the _get_cwd method cross platform, now it will run independent of the os

--- src/codevia/tools/workspace.py
+++ src/codevia/tools/workspace.py
@@ -1,11 +1,11 @@
-import subprocess
+import os
+
 class WorkspaceTools:
     def __init__(self):
         self.cwd = self._get_cwd()
 
     def _get_cwd(self):
-        output = subprocess.run("pwd", shell=True, capture_output=True, text=True, timeout=30)
-        return output.stdout + output.stderr
+        return os.getcwd()
 
 if __name__ == "__main__":
     tools = WorkspaceTools()
